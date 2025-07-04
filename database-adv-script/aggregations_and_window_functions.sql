5f9f68abaa1100e506b1a700de01effe85467bab
virginianjoki <njokivirginia0619@gmail.com>
2025-07-04 14:57:09 +0300

    joins_queries

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -12,32 +12,6 @@ GROUP BY
 ORDER BY 
     total_bookings DESC;
 
--- -- Rank properties based on number of bookings using RANK()
--- SELECT 
---     property_id,
---     COUNT(*) AS total_bookings,
---     RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
--- FROM 
---     bookings
--- GROUP BY 
---     property_id
--- ORDER BY 
---     booking_rank;
-
--- Query 1: Total bookings per user
-SELECT 
-    users.id AS user_id,
-    users.name AS user_name,
-    COUNT(bookings.id) AS total_bookings
-FROM 
-    users
-LEFT JOIN 
-    bookings ON users.id = bookings.user_id
-GROUP BY 
-    users.id, users.name
-ORDER BY 
-    total_bookings DESC;
-
 -- Query 2: Rank properties based on bookings using ROW_NUMBER()
 SELECT 
     property_id,
@@ -49,3 +23,5 @@ GROUP BY
     property_id
 ORDER BY 
     booking_rank;
+
+
