29cd24600a5c4a7227ebf5f29691eb58e3819baf
virginianjoki <njokivirginia0619@gmail.com>
2025-07-04 15:00:25 +0300

    joins_queries

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -12,15 +12,26 @@ GROUP BY
 ORDER BY 
     total_bookings DESC;
 
--- Query 2: Rank properties based on bookings using ROW_NUMBER()
+-- Query 2: Rank properties by total bookings using ROW_NUMBER()
 SELECT 
     property_id,
     COUNT(*) AS total_bookings,
-    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_row_number
+FROM 
+    bookings
+GROUP BY 
+    property_id
+ORDER BY 
+    booking_row_number;
+
+-- Query 3: Rank properties by total bookings using RANK()
+SELECT 
+    property_id,
+    COUNT(*) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
 FROM 
     bookings
 GROUP BY 
     property_id
 ORDER BY 
     booking_rank;
-
